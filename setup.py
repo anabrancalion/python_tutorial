405482109eae89f21d8fcb1687f971e368f74654
Ana Costa <acosta@cheyenne2.cheyenne.ucar.edu>
2021-09-15 14:03:40 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name-"mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="AFBC",
+    author_email="abc0112@auburn.edu",
+    packages=["mysci"],
+    install_requires=[])
+    
+    
